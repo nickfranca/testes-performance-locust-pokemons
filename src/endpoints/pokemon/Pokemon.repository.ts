9db4987a860aa4ed6014ce903fb79fa6c47f4bdc
davi-teixeira7 <davitx2005@gmail.com>
2025-11-24 15:10:44 -0300

    unificando branches

--- src/endpoints/pokemon/Pokemon.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { MysqlDataSource } from '../../config/database';
-import { Pokemon } from './Pokemon.entity';
-
-export class PokemonRepository extends Repository<Pokemon> {
-  constructor() {
-    super(Pokemon, MysqlDataSource.manager);
-  }
-}
